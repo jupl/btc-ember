de4e85ef0022738fe58d0d02a0cd28819dada332
Juan Placencia <juan.placencia.512@gmail.com>
2013-11-09 21:34:33 -0600

    Remove emblem and simplify generators (emblem may go into a fork)

--- brunch-config.js
+++ brunch-config.js
@@ -1,6 +1,13 @@
 var setup = require('./setup');
 
 exports.config = setup({
+  modules: {
+    definition: false,
+    wrapper: function(path, code) {
+      return ['(function() {', code, '})()'].join('\n');
+    }
+  },
+
   files: {
     javascripts: {
       joinTo: {
@@ -16,7 +23,12 @@ exports.config = setup({
     },
 
     templates: {
-      joinTo: 'javascripts/app.js'
+      joinTo: 'javascripts/app.js',
+      paths: {
+        jquery: 'bower_components/jquery/jquery.js',
+        ember: 'bower_components/ember/ember.js',
+        handlebars: 'bower_components/handlebars/handlebars.js'
+      }
     }
   }
 });
